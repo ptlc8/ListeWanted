bc125a1180d7f205a725e2f2c84a57b3ffffdc30
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-01-26 03:27:23 +0100

    fix onglet partenariats

--- app/src/main/java/fr/liste_wanted/ui/MainActivity.java
+++ app/src/main/java/fr/liste_wanted/ui/MainActivity.java
@@ -25,9 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_events, R.id.navigation_defis).build();
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_events, R.id.navigation_defis, R.id.navigation_partnership).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
