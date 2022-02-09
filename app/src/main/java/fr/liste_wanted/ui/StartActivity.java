b3fb9b4f438d79bf226b080cefc0ab27494eaa41
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-02-09 04:37:12 +0100

    ajout de l'animation du logo au démarrage

--- app/src/main/java/fr/liste_wanted/ui/StartActivity.java
+++ app/src/main/java/fr/liste_wanted/ui/StartActivity.java
@@ -1,12 +1,20 @@
 package fr.liste_wanted.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,17 +28,44 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        //Glide.with(this).load(R.drawable.ic_logo_animated).into(((ImageView)findViewById(R.id.imageView)));
+        Glide.with(this).asGif()
+            .load(R.drawable.ic_wanted_logo_animated)
+            .listener(new RequestListener<GifDrawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                    startApp();
+                    return false;
+                }
+                @Override
+                public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                    resource.setLoopCount(1);
+                    resource.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                        @Override
+                        public void onAnimationEnd(Drawable drawable) {
+                            startApp();
+                            super.onAnimationEnd(drawable);
+                        }
+                    });
+                    return false;
+                }
+            })
+            .into(((ImageView)findViewById(R.id.imageView)));
 
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+
+        new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(StartActivity.this, MainActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
-                finish();
+                startApp();
             }
-        }, 1000);
+        }, 4200);
+    }
+
+    private boolean hasStart = false;
+    private void startApp() {
+        if (hasStart) return;
+        hasStart = true;
+        startActivity(new Intent(StartActivity.this, MainActivity.class));
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        finish();
     }
 }
